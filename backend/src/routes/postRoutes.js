dd100c8146e3722f75e23e2a20faf2d5d11b5db7
Kundan-CR7 <Kundangupta7334@gmail.com>
2025-11-23 04:44:47 +0530

    implemented get and update specific user posts

--- backend/src/routes/postRoutes.js
+++ backend/src/routes/postRoutes.js
@@ -1,11 +1,13 @@
 import express from "express";
-import { createPost, getPosts, getPostById } from "../controllers/postController.js";
+import { createPost, getPosts, getPostByPostId,getPostByAuthorId,updatePost } from "../controllers/postController.js";
 
 const router = express.Router();
 
 router.post("/create", createPost);
 router.get('/',getPosts)
-router.get('/id/:id', getPostById)
+router.get('/id/:id', getPostByPostId)
+router.get('/author/:id', getPostByAuthorId)
 router.get('/:category',getPosts)
+router.put("/update/:id",updatePost)
 
 export default router;
